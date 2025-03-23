d7e8582bba2feb8ddaa39e09299d7d6bc25c6ea8
raulmateos-m <raul.mateos@gmail.com>
2025-03-23 01:41:32 +0100

    Add files via upload

--- scripts/raul.js
+++ scripts/raul.js
@@ -4,16 +4,19 @@ $(function() {
 		$navb = $('#navb'),
 		$nav2 = $('#nav2'),
 		$ind = $('#ind'),
+		$clr = $('#clr'),
+		$msg2 = $('#msg2'),
 		$navt = $('#nav-toggle'),
+		$navtc = $navt.add($nav).add($navb),
 		$input = $('input'),
-		$tabla = $('.tablesorter').tablesorter(),
-		$total = $('.tablesorter tbody tr'),
-		totalRecords = $total.length;
-	let boot='. The dates of <span class="b">bootlegs</span> are dd/mm/yy.';
-	let records = 'records'	
-	let spec='. The records listed on specific pages are not counted here';
-	let specCD='. The CD listed on specific pages are not counted here';
-	let updated='. Record collection updated March 2025';
+		$tabla = $('.tablesorter'),
+		$total = $tabla.find('tbody tr'),
+		$tableSorterInstance = $tabla.tablesorter();
+	let records = 'records',
+		boot='. The dates of <span class="b">bootlegs</span> are dd/mm/yy',
+		spec='. The records listed on specific pages are not counted here',
+		specCD='. The CD listed on specific pages are not counted here',
+		updated='. Record collection updated March 2025';
 	const menuItems={
 		'rainbow':[
 			'<li><a href="vinyl.html">Vinyl</a></li>',
@@ -31,28 +34,40 @@ $(function() {
 		]
 	};
 	function updateNav(elem, page, id) {
-	    let selector = page.includes('.html') ? `a[href='${page}']` : `a[href*='${page}']`;
-	    const $link = elem.find(selector);
-	    $link.parent().attr("id", id).html($link.text());
+		let selector = page.includes('.html') ? `a[href='${page}']` : `a[href*='${page}']`;
+		const $link = elem.find(selector);
+		$link.parent().attr("id", id).html($link.text());
 	}
 	function addSection(name,rid){$nav2.append(`<li><a href="#${rid}">${name}</a></li>`);}
 	function getRecordInfo(found,terms){
 		if (terms =='CD') {return '';
-		} else if (terms=='7" singles,12" singles,LP'){
-			return '(' + terms.map(term => 
-		`${term}: <span class="c">${found.find(`td:nth-child(5):contains(${term.replace('singles', '')})`).length}</span>`
-		).join('; ') + ')';
-		} else
-			return '(' + terms.map(term => 
-		`${term}: <span class="c">${found.find(`td:nth-child(4):contains(${term.replace('singles', '')})`).length}</span>`
-		).join('; ') + ')';
+		} else {
+			let columnIndex = 4;
+			if (terms == '7" singles,12" singles,LP') {columnIndex = 5;}
+			let cellTexts = [];
+			found.each(function() {
+				let cell = $(this).find(`td:nth-child(${columnIndex})`);
+				if (cell.length) {
+					cellTexts.push(cell.text());
+				}
+			});
+			let counts = {};
+			terms.forEach(term => {
+				let searchTerm = term.replace('singles', '');
+				counts[term] = 0;
+				cellTexts.forEach(text => {
+					if (text.indexOf(searchTerm) !== -1) {counts[term]++;}
+				});
+			});
+			return '(' + terms.map(term => `${term}: <span class="c">${counts[term]}</span>`).join('; ') + ')';
+		}
 	}
 	function getRecordInfoByPath(found, pathname) {
 		if (pathname.includes('bootlegs')) {
 			return getRecordInfo(found, ['7" singles', 'LP', 'CD']) + boot + updated;
 		} else if (pathname.includes('rainbow')) {
 			if (pathname.includes('vinyl')) {
-		 		return getRecordInfo(found, ['7" singles', 'LP']) + updated;
+				return getRecordInfo(found, ['7" singles', 'LP']) + updated;
 			} else if (pathname.includes('CD')) {
 				return getRecordInfo(found, ['CD', 'DVD']) + updated;
 			} else {
@@ -69,7 +84,7 @@ $(function() {
 		}
 	}
 	function updateMsgText(found,pathname,targetMsg,msgText){
-		if (targetMsg=='msg2'){boot=''; spec=''; specCD=''; updated=''}
+		if (targetMsg=='msg2'){boot=spec=specCD=updated='';}
 		$(`#${targetMsg}`).html(msgText + (found.length !== 0 ? getRecordInfoByPath(found, pathname) : ''))
 	}
 	function updateNavandVars(page,id){
@@ -86,19 +101,19 @@ $(function() {
 		pagename = 'iron_maiden';
 	} else if (pathname.includes('CD.html')){
 		records = 'CD';
-		$('.tablesorter td:first-child').addClass('bo');
+		$tabla.find('td:first-child').addClass('bo');
 		n='2b';
 	} else if (pathname.includes('vinyl.html')){
-		$('.tablesorter td:first-child').addClass('bo');
+		$tabla.find('td:first-child').addClass('bo');
 		n='2b';
 	}
-	$navt.on('click', () => $navt.add($nav).add($navb).toggleClass('collapsed'));
+	$navt.on('click', () => $navtc.toggleClass('collapsed'));
 	$(document).on('keyup',evt => {
-		if (evt.keyCode === 27 && $nav.hasClass('collapsed')) {
-			$navt.add($nav).add($navb).toggleClass('collapsed');
+		if (evt.key === 'Escape' && $nav.hasClass('collapsed')) {
+			$navtc.toggleClass('collapsed');
 		}
 	});
-	$nav.append(`
+	const mainMenuHTML =`
 	<li><a href="${path}rainbow/vinyl.html">Rainbow (Dio)</a></li>
 	<li><a href="${path}iron_maiden/singles.html">Iron Maiden</a></li>
 	<li><a href="${path}deep_purple.html">Deep Purple</a></li>
@@ -106,34 +121,41 @@ $(function() {
 	<li><a href="${path}dio.html">DIO</a></li>
 	<li><a href="${path}vinyl.html">Vinyl Collection</a></li>
 	<li><a href="${path}CD.html">CD Collection</a></li>
- 	`);
+	`;
+	$nav.append(mainMenuHTML);
 	updateNav($nav,pagename,'page'+n);
 	$('section').each(function(){addSection($(this).find('h3').first().text(), $(this).attr('id'));});
-
+	const totalRecords = $total.length;
 	updateMsgText($total,pathname,'msg',`Total ${records}: <span class="bo">${totalRecords}</span> `);
 	$input.attr('placeholder',`Type here to search in the ${totalRecords} items`);
-	$('#clr').on('click',function(){$input.val('').focus().trigger({type:'keyup',Code:'Backspace',keyCode:8});$('#msg2').html('&nbsp;');});
-	$('.tablesorter td:nth-child(2)').addClass('n');
-	$('.tablesorter td:nth-child(3)').addClass('c');
-	$('.tablesorter tr:nth-child(odd)').addClass('even');
+	$clr.on('click',function(){$input.val('').focus().trigger({type:'keyup',Code:'Backspace',keyCode:8});$msg2.html('&nbsp;');});
+	$tabla.find('td:nth-child(2)').addClass('n');
+	$tabla.find('td:nth-child(3)').addClass('c');
+	$tabla.find('tbody tr:nth-child(odd)').addClass('even');
+	let indexHTML = '';
 	for (let i = 65; i <= 90; i++) {
-		$ind.append(`<a href="#${String.fromCharCode(i).toUpperCase()}">${String.fromCharCode(i).toUpperCase()}</a> `);
+		const letter = String.fromCharCode(i).toUpperCase();
+		indexHTML += `<a href="#${letter}">${letter}</a> `;
 	}
-	$ind.append('<a href="#V/A">Compilations</a>');
+	indexHTML += '<a href="#V/A">Compilations</a>';
+	$ind.append(indexHTML);
 	$('#fil input').on('keyup',function(){
-		$.uiTableFilter($tabla,this.value);
+		$.uiTableFilter($tableSorterInstance,this.value);
 		$('section:hidden').show();
-		let found=$('.tablesorter > tbody > tr:visible');
-		let msg2Text=found.length===0 ? `No ${records} found` : `<span class="bo">${found.length}</span> ${records} found `;
+		const found=$tabla.find('tbody > tr:visible');
+		const msg2Text=found.length===0 ? `No ${records} found` : `<span class="bo">${found.length}</span> ${records} found `;
 		updateMsgText(found,pathname,'msg2',msg2Text);
-		$('.tablesorter').each(function(){
+		$tabla.each(function(){
 			$(this).parent().toggle($('tbody > tr:visible', this).length > 0);
 		});
 	});
-	const columnWidths={'Artist':'100px','Title':'170px','City':'110px','Country':'110px','Label':'185px'};
-	$.each(columnWidths, (columnName, width) => $(`th.header:contains('${columnName}')`).css('width', width));
-	$('c').replaceWith(function(){return `<span class="c">${$(this).html()}</span>; `;});
-	$('w').replaceWith(function(){return `<span class="w">${$(this).html()}</span>`;});
+	function replaceElement(selector, className) {
+		$(selector).replaceWith(function() {
+			return `<span class="${className}">${$(this).html()}</span>${className === 'c' ? '; ' : ''}`;
+		});
+	}
+	replaceElement('c', 'c');
+	replaceElement('w', 'w');
 	$('.s').append('<a href="#toc"> <i class="icon-long-arrow-up"></i></a>');
 	$('#up').prepend('<a href="#toc">Go Up</a>&nbsp;');
 	$('header').attr('id','toc');
