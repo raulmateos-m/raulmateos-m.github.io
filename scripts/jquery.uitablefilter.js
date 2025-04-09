228afcbc8b1df90a63c6a07fdd5107944465ac04
raulmateos-m <raul.mateos@gmail.com>
2025-04-09 23:58:22 +0200

    Update jquery.uitablefilter.js

--- scripts/jquery.uitablefilter.js
+++ scripts/jquery.uitablefilter.js
@@ -1,44 +1,72 @@
 $(function() {
-	$.uiTableFilter = function(jq, phrase, column, ifHidden){
+	$.uiTableFilter = function($table, phrase, column, ifHidden) {
 		var new_hidden = false;
-		if( this.last_phrase === phrase ) return false;
-		var phrase_length = phrase.length;
+		if ($.uiTableFilter.last_phrase === phrase) return false;
 		var words = phrase.toLowerCase().split(" ");
-		var matches = function(elem) { elem.show() }
-		var noMatch = function(elem) { elem.hide(); new_hidden = true }
-		var getText = function(elem) { return elem.text() }
-		if( column ) {
-			var index = null;
-			jq.find("thead > tr:last > th").each( function(i){
-				if( $.trim($(this).text()) == column ){index = i; return false;}
-			});
-			if( index == null ) throw("given column: " + column + " not found")
-			getText = function(elem){ return $(elem.find("td:eq(" + index + ")")).text()}
+		var hideElement = function($elem) { $elem.hide(); new_hidden = true; };
+		var showElement = function($elem) { $elem.show(); };
+		var getText;
+		var columnIndex = null;
+		var $theadLastRowTh = $table.find("thead > tr:last > th");
+		if (column) {
+			if (!$.uiTableFilter.columnIndices) {
+				$.uiTableFilter.columnIndices = {};
+			}
+			if ($.uiTableFilter.columnIndices[column] === undefined) {
+				$theadLastRowTh.each(function(i) {
+					if ($.trim($(this).text()) === column) {
+						$.uiTableFilter.columnIndices[column] = i;
+						return false;
+					}
+				});
+			}
+			columnIndex = $.uiTableFilter.columnIndices[column];
+			if (columnIndex === undefined) {
+				throw "given column: " + column + " not found";
+			}
+			getText = function($row) {
+				return $row.find("td").eq(columnIndex).text();
+			};
+		} else {
+			getText = function($elem) { return $elem.text(); };
 		}
-		if( (words.size > 1) && (phrase.substr(0, phrase_length - 1) === this.last_phrase) ) {
-			if( phrase[-1] === " " ) { this.last_phrase = phrase; return false; }
-			var words = words[-1];
-			matches = function(elem) {;}
-			var elems = jq.find("tbody:first > tr:visible")
-		}
-		else {
+
+		var $rows;
+		if (words.length > 1 && phrase.slice(0, -1) === $.uiTableFilter.last_phrase) {
+			if (phrase.slice(-1) === " ") {
+				$.uiTableFilter.last_phrase = phrase;
+				return false;
+			}
+			words = [words[words.length - 1]];
+			showElement = function($elem) {};
+			$rows = $table.find("tbody:first > tr:visible");
+		} else {
 			new_hidden = true;
-			var elems = jq.find("tbody:first > tr")
+			$rows = $table.find("tbody:first > tr");
 		}
-		elems.each(function(){
-			var elem = $(this);
-			$.uiTableFilter.has_words( getText(elem), words, false ) ? matches(elem) : noMatch(elem);
+		$rows.each(function() {
+			var $row = $(this);
+			if ($.uiTableFilter.has_words(getText($row), words, false)) {
+				showElement($row);
+			} else {
+				hideElement($row);
+			}
 		});
-		last_phrase = phrase;
-		if( ifHidden && new_hidden ) ifHidden();
-		return jq;
- 	};
-	$.uiTableFilter.last_phrase = ""
-	$.uiTableFilter.has_words = function( str, words, caseSensitive ) {
+		$.uiTableFilter.last_phrase = phrase;
+		if (ifHidden && new_hidden) {
+			ifHidden();
+		}
+		return $table;
+	};
+	$.uiTableFilter.last_phrase = "";
+	$.uiTableFilter.columnIndices = {};
+	$.uiTableFilter.has_words = function(str, words, caseSensitive) {
 		var text = caseSensitive ? str : str.toLowerCase();
-		for (var i=0; i < words.length; i++) {
-			if (text.indexOf(words[i]) === -1) return false;
+		for (var i = 0; i < words.length; i++) {
+			if (text.indexOf(words[i]) === -1) {
+				return false;
+			}
 		}
 		return true;
-	}
+	};
 });
